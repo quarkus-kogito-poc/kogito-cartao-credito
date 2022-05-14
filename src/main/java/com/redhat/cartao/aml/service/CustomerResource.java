0e3ce837c0ed79ebdc605bba29a8654dfd69c0ea
Gabriel Sampaio <gsampaio@redhat.com>
2022-05-14 00:28:32 -0300

    fixing package

--- src/main/java/com/redhat/cartao/aml/service/CustomerResource.java
+++ src/main/java/com/redhat/cartao/aml/service/CustomerResource.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.redhat.cartao.cartao.service;
+package com.redhat.cartao.aml.service;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
